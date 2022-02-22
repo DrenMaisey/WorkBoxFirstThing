0087d145a84836e3c0ae1690453ca829bd545168
DrenMaisey <31235775+DrenMaisey@users.noreply.github.com>
2022-02-22 14:10:07 +0100

    .

--- Install.ps1
+++ Install.ps1
@@ -1,23 +1,25 @@
-winget install 7zip
+winget install --id=7zip.7zip
 winget install Mozilla.Firefox
-winget install adobereader
+winget install --id=Adobe.Acrobat.Reader.64-bit
 winget install VideoLAN.VLC
-winget install f.lux
 winget install Microsoft.PowerToys
 winget install pdfsam
 winget install "cutepdf writer"
 winget install Docker.DockerDesktop
-winget install Slack
+winget install SlackTechnologies.Slack
 winget install Telegram.TelegramDesktop
 winget install BraveSoftware.BraveBrowser
 winget install KeepassXC
-winget install joplin
+winget install joplin.joplin
 winget install Microsoft.VisualStudioCode
-winget install Microsoft.WindowsTerminal
 winget install Devolutions.RemoteDesktopManagerFree
 winget install Notepad++.Notepad++
 winget install greenshot
 winget install winscp
+winget install FedericoTerzi.espanso
+winget install Notion.Notion
+winget install TheDocumentFoundation.LibreOffice
+winget install TeamViewer.TeamViewer
 Install-Module PSWindowsUpdate
 Get-Windowsupdate
 Install-WindowsUpdate
